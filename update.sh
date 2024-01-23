5bf67a53f6f92e0eedeca1cdc4659500edddd0e0
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-01-23 09:06:36 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 
 
 sudo rm -r /home/pi/DCMLocker
-sudo rm -r /home/pi/'DCMLocker\base'
+sudo rm -r /home/pi/'DCMLocker\Base'
 sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
 sudo systemctl restart dcmlocker
 sudo reboot
