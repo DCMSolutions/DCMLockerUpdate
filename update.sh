5ac664b2fc66cce36ea656bebb1a384c17795c7c
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-04-26 10:38:44 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,4 +6,5 @@ sudo rm -r /home/pi/DCMLocker
 sudo rm -r /home/pi/'DCMLocker\Base'
 sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
 sudo systemctl restart dcmlocker
+sudo reboot
 sudo chromium-browser http://localhost:5022/
