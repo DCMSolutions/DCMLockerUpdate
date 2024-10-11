64e400c1b0b15e344edb00e13e67e94aecea5727
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-10-11 09:24:12 -0300

    sacar chromium update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,4 @@ sudo rm -r /home/pi/DCMLocker.bak
 sudo mv /home/pi/DCMLocker /home/pi/DCMLocker.bak
 sudo rm -r /home/pi/'DCMLocker\Base'
 sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
-sudo apt update 
-sudo apt upgrade chromium-browser -y
 sudo reboot
