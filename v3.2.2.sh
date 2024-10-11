26cfc4710926165922748169fb1afcd081ce2e50
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-10-11 10:16:27 -0300

    Fix v3.2.2.sh

--- v3.2.2.sh
+++ v3.2.2.sh
@@ -3,5 +3,5 @@ sudo mv /home/pi/DCMLocker /home/pi/DCMLocker.bak
 sudo rm -r /home/pi/'DCMLocker\Base'
 sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
 cd /home/pi/DCMLocker
-git checkout 8e22b6029f52858ce64367433d88423b6ae1a63c
+sudo git checkout -f 8e22b6029f52858ce64367433d88423b6ae1a63c
 sudo reboot
