d1bb24183841b38b50460b5698d5e19a5b50c40a
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-10-22 00:56:25 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 sudo rm -r /home/pi/DCMLocker.bak
 
 sudo cp /home/pi/DCMLocker/eventos-$(date +"%m-%Y").ans /home/pi/eventos-$(date +"%m-%Y").ans
+ cp /home/pi/DCMLocker/eventos-$(date --date="$(date +%Y-%m-15) -1 month" +"%m-%Y").ans /home/pi/eventos-$(date --date="$(date +%Y-%m-15) -1 month" +"%m-%Y").ans
 sudo cp /home/pi/'DCMLocker\Base'/LoackerConfig.config /home/pi/LoackerConfig.config
 sudo cp /home/pi/'DCMLocker\Base'/LoackerMap.map /home/pi/LoackerMap.map
 
