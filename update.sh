65678a8ecc50526ea7d1f7e85e5ea199c2f49890
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-10-22 01:05:02 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,4 +10,8 @@ sudo cp /home/pi/'DCMLocker\Base'/LoackerMap.map /home/pi/LoackerMap.map
 sudo mv /home/pi/DCMLocker /home/pi/DCMLocker.bak
 sudo rm -r /home/pi/'DCMLocker\Base'
 sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
+
+sudo cp /home/pi/eventos-$(date +"%m-%Y").ans /home/pi/DCMLocker/eventos-$(date +"%m-%Y").ans 
+sudo cp /home/pi/eventos-$(date --date="$(date +%Y-%m-15) -1 month" +"%m-%Y").ans /home/pi/DCMLocker/eventos-$(date --date="$(date +%Y-%m-15) -1 month" +"%m-%Y").ans 
+
 sudo reboot
