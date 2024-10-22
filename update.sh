4491dea02d9a70d2b0668550d5f8fbdc2796adcc
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-10-22 00:34:02 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,10 @@
 
 sudo rm -r /home/pi/DCMLocker.bak
 sudo mv /home/pi/DCMLocker /home/pi/DCMLocker.bak
+
+sudo mv /home/pi/'DCMLocker\Base'/LoackerConfig.config /home/pi/LoackerConfig.config
+sudo mv /home/pi/'DCMLocker\Base'/LoackerMap.map /home/pi/LoackerMap.map
+
 sudo rm -r /home/pi/'DCMLocker\Base'
 sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
 sudo reboot
