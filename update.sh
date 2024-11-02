f393533c0502f02922e68ec0d2595ed0bdab9766
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-11-02 18:48:01 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,8 +2,6 @@
 
 sudo rm -r /home/pi/DCMLocker.bak
 
-sudo cp /home/pi/DCMLocker/eventos-$(date +"%m-%Y").ans /home/pi/eventos-$(date +"%m-%Y").ans
-sudo cp /home/pi/DCMLocker/eventos-$(date --date="$(date +%Y-%m-15) -1 month" +"%m-%Y").ans /home/pi/eventos-$(date --date="$(date +%Y-%m-15) -1 month" +"%m-%Y").ans
 sudo cp /home/pi/'DCMLocker\Base'/LoackerConfig.config /home/pi/LoackerConfig.config
 sudo cp /home/pi/'DCMLocker\Base'/LoackerMap.map /home/pi/LoackerMap.map
 
@@ -11,7 +9,7 @@ sudo mv /home/pi/DCMLocker /home/pi/DCMLocker.bak
 sudo rm -r /home/pi/'DCMLocker\Base'
 sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
 
-sudo cp /home/pi/eventos-$(date +"%m-%Y").ans /home/pi/DCMLocker/eventos-$(date +"%m-%Y").ans 
-sudo cp /home/pi/eventos-$(date --date="$(date +%Y-%m-15) -1 month" +"%m-%Y").ans /home/pi/DCMLocker/eventos-$(date --date="$(date +%Y-%m-15) -1 month" +"%m-%Y").ans 
+sudo cp /home/pi/LoackerConfig.config /home/pi/'DCMLocker\Base'/LoackerConfig.config
+sudo cp /home/pi/LoackerMap.map /home/pi/'DCMLocker\Base'/LoackerMap.map
 
 sudo reboot
