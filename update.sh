dcefac76f58b2028854945be8862c0937cefba4a
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2024-11-29 03:01:32 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,6 +7,14 @@ if [ ! -f /home/pi/LoackerMap.map ]; then
     sudo cp /home/pi/'DCMLocker\Base'/LoackerMap.map /home/pi/LoackerMap.map
 fi
 
+#esto le da acceso a la app a modificar los archivos
+sudo chown pi:pi /home/pi/LoackerMap.map
+sudo chown pi:pi /home/pi/LoackerConfig.config
+
+#esto mata el servicio que abria anteriormente el chromium
+sudo rm -f /etc/xdg/autostart/display.desktop
+sudo rm -f /etc/xdg/autostart/display.desktop.save
+
 sudo rm -r /home/pi/DCMLocker.bak
 sudo mv /home/pi/DCMLocker /home/pi/DCMLocker.bak
 sudo rm -r /home/pi/'DCMLocker\Base'
