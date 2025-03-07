f6ffae31839a83ddcdb549f8cc710df99abc2084
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2025-03-07 13:43:14 -0300

    Create v3.6.7.sh
    Creando archivo de version 3.6.7. Para poder actualizar

---
+++ v3.6.7.sh
@@ -0,0 +1,7 @@
+sudo rm -r /home/pi/DCMLocker.bak
+sudo mv /home/pi/DCMLocker /home/pi/DCMLocker.bak
+sudo rm -r /home/pi/'DCMLocker\Base'
+sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
+cd /home/pi/DCMLocker
+sudo git checkout -f 3c2da4c9fbd61494bf054a1226baf456472ba990
+sudo reboot
