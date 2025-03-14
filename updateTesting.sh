77a82b871de56a78d9f77a71092d8a52b133ce86
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2025-03-14 10:16:06 -0300

    Update updateTesting.sh

--- updateTesting.sh
+++ updateTesting.sh
@@ -1,5 +1,5 @@
 sudo rm -r /home/pi/DCMLocker.bak
 sudo mv /home/pi/DCMLocker /home/pi/DCMLocker.bak
 sudo rm -r /home/pi/'DCMLocker\Base'
-sudo git clone https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
+sudo git clone --branch testing https://github.com/DCMSolutions/DCMLockerLast /home/pi/DCMLocker
 sudo reboot --force --reboot
