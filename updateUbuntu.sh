14dd2a5808f1ae70d01fb7d3d85c2b056a39857d
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2025-10-04 18:31:36 -0300

    Update updateUbuntu.sh

--- updateUbuntu.sh
+++ updateUbuntu.sh
@@ -2,4 +2,4 @@ sudo rm -r /home/DCMLocker.bak
 sudo mv /home/DCMLocker /home/DCMLocker.bak
 sudo rm -r /home/'DCMLocker\Base'
 sudo git clone https://github.com/DCMSolutions/DCMLockerLastUbuntu /home/DCMLocker
-sudo reboot --force --reboot
+sudo reboot
