6aa95ef778722c628bcdbdd68bf9925870ace339
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2025-11-04 14:22:55 -0300

    Update updateUbuntu.sh

--- updateUbuntu.sh
+++ updateUbuntu.sh
@@ -1,5 +1,12 @@
-sudo rm -r /home/DCMLocker.bak
-sudo mv /home/DCMLockerLastUbuntu /home/DCMLocker.bak
-sudo rm -r /home/'DCMLockerLastUbuntu\Base'
-sudo git clone https://github.com/DCMSolutions/DCMLockerLastUbuntu
-sudo reboot
+#!/usr/bin/env bash
+
+cd /home || exit 1
+
+rm -rf /home/DCMLocker.bak
+[ -d /home/DCMLockerLastUbuntu ] && mv /home/DCMLockerLastUbuntu /home/DCMLocker.bak || true
+
+rm -rf /home/'DCMLockerLastUbuntu\Base' || true
+
+git clone --depth 1 https://github.com/DCMSolutions/DCMLockerLastUbuntu /home/DCMLockerLastUbuntu
+
+nohup bash -c 'sleep 2; reboot' >/dev/null 2>&1 &
