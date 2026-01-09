107e93dc9d11d83025d1fd4c2aad93bc1e56dbce
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2026-01-09 15:25:31 -0300

    Update updateUbuntu.sh

--- updateUbuntu.sh
+++ updateUbuntu.sh
@@ -5,8 +5,8 @@ find /home -maxdepth 1 -type f -name 'eventos-*' -print -delete
 
 rm -rf /home/DCMLockerLastUbuntu
 
-git clone https://github.com/DCMSolutions/DCMLockerLastUbuntu.git /home/DCMLockerLastUbuntu
+git clone --depth 1 https://github.com/DCMSolutions/DCMLockerLastUbuntu.git /home/DCMLockerLastUbuntu
 
 sleep 1
-
+sync
 reboot
