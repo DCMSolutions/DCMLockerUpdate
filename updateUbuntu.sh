24dda98934a3d3f7757a7200911e9551800b58c6
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2026-01-09 15:23:57 -0300

    Update updateUbuntu.sh

--- updateUbuntu.sh
+++ updateUbuntu.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo find /home -maxdepth 1 -type f -name 'eventos-*' -print -delete
+find /home -maxdepth 1 -type f -name 'eventos-*' -print -delete
 
-sudo rm -rf /home/DCMLockerLastUbuntu
+rm -rf /home/DCMLockerLastUbuntu
 
-sudo git clone https://github.com/DCMSolutions/DCMLockerLastUbuntu.git /home/DCMLockerLastUbuntu
+git clone https://github.com/DCMSolutions/DCMLockerLastUbuntu.git /home/DCMLockerLastUbuntu
 
 sleep 1
 
-sudo reboot
+reboot
