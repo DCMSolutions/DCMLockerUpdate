b21e0e1a8f12d8f3f5b7e677c853ccf016249bc8
DCMSolutions <123087299+DCMSolutions@users.noreply.github.com>
2026-01-13 12:52:33 -0300

    Update updateUbuntu.sh

--- updateUbuntu.sh
+++ updateUbuntu.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ERROR_LOG="/home/error.ans"
-RUN_LOG="/home/run.ans"
-
-exec >>"$RUN_LOG" 2>&1
-
+exec >>/home/run.ans 2>&1
 trap 'rc=$?; printf "[%s] ERROR: comando=%q | linea=%s | exit=%s\n" \
-  "$(date -Is)" "$BASH_COMMAND" "$LINENO" "$rc" >> "$ERROR_LOG"; exit "$rc"' ERR
+  "$(date -Is)" "$BASH_COMMAND" "$LINENO" "$rc" >> /home/error.ans; exit "$rc"' ERR
 
 echo "=== Inicio: $(date -Is) ==="
-echo "pwd inicial: $(pwd)"
 
-# Muy importante: salir de cualquier ruta que pueda ser borrada
-cd / || exit 1
-echo "pwd forzado: $(pwd)"
+cd /home
 
 find /home -maxdepth 1 -type f -name 'eventos-*' -print -delete
 
-rm -rf /home/DCMLockerLastUbuntu
+rm -rf /home/DCMLockerLastUbuntu.new
+git clone --depth 1 https://github.com/DCMSolutions/DCMLockerLastUbuntu.git /home/DCMLockerLastUbuntu.new
 
-git clone --depth 1 https://github.com/DCMSolutions/DCMLockerLastUbuntu.git /home/DCMLockerLastUbuntu
+rm -rf /home/DCMLockerLastUbuntu.old
+mv /home/DCMLockerLastUbuntu /home/DCMLockerLastUbuntu.old
+mv /home/DCMLockerLastUbuntu.new /home/DCMLockerLastUbuntu
 
-sleep 1
 sync
 reboot
